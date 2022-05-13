faaf98ede91dcc6a8371831f969efd79039c65c2
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-13 11:24:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -26,7 +26,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
   int isSocInvalid = checklimit(soc,20,80);
   printf("Charge Rate ");
   int isChargerateInvalid= checklimit(chargeRate,0,0.8);
-  //int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
+  int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
   if(isTemperatureInvalid||isSocInvalid||isChargerateInvalid==1)
     printf("Battery's condition is not okay\n");
   //else
