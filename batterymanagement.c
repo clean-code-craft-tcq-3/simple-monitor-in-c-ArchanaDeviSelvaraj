ec69ee5f208835a2a47f5d56007f882a62a2344e
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 16:09:50 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -9,7 +9,6 @@ int CheckBatteryStatus(float temperature, float soc, float chargeRate) {
   printf("Charge Rate ");
   int isChargerateInvalid= checkvaluebreach(chargeRate,0,0.8);
   int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
-  printf("%d\n",invalid);
   earlywarning(invalid,temperature,soc,chargeRate);
   return invalid; 
 }
