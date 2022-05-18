22abb8eab7b84d0493a9923252e61367b57d76a6
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 12:58:10 +0530

    Update checklimit.c

--- checklimit.c
+++ checklimit.c
@@ -1,1 +1,17 @@
-
+int checklimit(float value,float minlimit,float maxlimit)
+{
+  int alertcount=0;
+  if (value < minlimit) {
+    printf("value is below the limit\n");
+    alertcount=1;
+  }
+  else if (value > maxlimit){
+    printf("value is above the limit\n");
+    alertcount=1;
+  }
+  else
+  {
+    printf("value is within range\n");
+  }
+ return alertcount;
+}
