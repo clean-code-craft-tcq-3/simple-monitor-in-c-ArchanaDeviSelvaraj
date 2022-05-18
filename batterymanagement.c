948a5bd9cd8ecbd4eb1759d489a6d11f24b07ba7
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 15:19:03 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -6,24 +6,14 @@ int CheckBatteryStatus(float temperature, float soc, float chargeRate) {
   int isTemperatureInvalid = checklimit(temperature,0,45);
   int isTempApproachinglimit = warning(temperature,0,45);
   (isTempApproachinglimit==1) ? printf("Warning: Battery Temperature approaching lower limit\n") : ((isTempApproachinglimit==2)? printf("Warning: Battery Temperature approaching higher limit\n") : printf(""));
-  //if(isTempApproachinglimit==1)
-    //printf("Warning: Battery Temperature approaching lower limit\n");
-  //else if (isTempApproachinglimit==2)
-    //printf("Warning: Battery Temperature approaching higher limit\n");
   printf("State of Charge ");
   int isSocInvalid = checklimit(soc,20,80);
   int isSocApproachinglimit = warning(soc,20,80);
-  if(isSocApproachinglimit==1)
-    printf("Warning: Battery Approaching discharge\n");
-  else if (isSocApproachinglimit==2)
-    printf("Warning: Battery Approaching charge-peak\n");
+  (isSocApproachinglimit==1) ? printf("Warning: Battery Approaching discharge\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Approaching charge-peak\n") : printf(""));
   printf("Charge Rate ");
   int isChargerateInvalid= checklimit(chargeRate,0,0.8);
   int isChargerateApproachinglimit = warning(chargeRate,0,0.8);
-  if(isChargerateApproachinglimit==1)
-    printf("Warning: Battery Charge rate approaching lower limit\n");
-  else if (isChargerateApproachinglimit==2)
-    printf("Warning: Battery Charge rate approaching higher limit\n");
+  (isChargerateApproachinglimit==1) ? printf("Warning: Battery Charge rate approaching lower limit\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Charge rate approaching higher limit\n") : printf(""));
   int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
   return invalid;
 }
