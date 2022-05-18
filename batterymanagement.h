edaae28104331c5f9ca3a12198df06436810a3a5
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 13:03:05 +0530

    Update batterymanagement.h

--- batterymanagement.h
+++ batterymanagement.h
@@ -1,3 +1,3 @@
-int batteryIsOk(float temperature, float soc, float chargeRate);
+int CheckBatteryStatus(float temperature, float soc, float chargeRate);
 int warning(float value,float minlimit,float maxlimit);
 int checklimit(float value,float minlimit,float maxlimit);
