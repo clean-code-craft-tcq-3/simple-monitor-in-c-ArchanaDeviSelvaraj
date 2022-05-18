120913d592da079330cb7420ebdbc23e998bf7e8
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 14:42:02 +0530

    Update warning.c

--- warning.c
+++ warning.c
@@ -1,12 +1,11 @@
 int warning(float value,float minlimit,float maxlimit)
 {
   float tolerance=(5.000000/100.000000)*maxlimit;
-  //if(value>=minlimit && value<=minlimit+tolerance)
   if(minlimit<=value<=minlimit+tolerance)
   {
     return 1;
     }
-  else if(value<=maxlimit && value>=maxlimit-tolerance){
+  else if(maxlimit-tolerance<=value<=maxlimit){
     return 2;
     }
   else
