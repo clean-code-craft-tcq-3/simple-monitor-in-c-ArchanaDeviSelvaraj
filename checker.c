564c6ba02b6c9458bb8b306f0fea598f4e632b84
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 11:15:35 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,12 +5,12 @@ int warning(float value,float minlimit,float maxlimit)
 {
   float tolerance=(5.000000/100.000000)*maxlimit;
   if (value == minlimit||value == minlimit+tolerance) {
-    return 1;
     printf("Low limit warning\n");
+    return 1;
     }
   else if (value == maxlimit||value == maxlimit-tolerance){
-    return 2;
     printf("High limit warning\n");
+    return 2;
     }
   else
     return 0;
