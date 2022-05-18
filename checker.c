23ed43344c267852f7fcdad518130dfc67f2b9d0
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 12:24:59 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -60,26 +60,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 }
 
 int main() {
-  printf("Check 1\n");
-  assert(batteryIsOk(23, 70, 0.7)==0);
-  printf("\nCheck 2\n");
-  assert(batteryIsOk(46, 81, 0.9)==1);
-  printf("\nCheck 3\n");
-  assert(batteryIsOk(-1, 19, -1)==1);
-  printf("\nCheck 4\n");
-  assert(batteryIsOk(30, 50, 0.4)==0);
-  printf("\nCheck 5\n");
-  assert(batteryIsOk(35, 55, 0.3)==0);
-  printf("\nCheck 6\n");
-  assert(batteryIsOk(-10, 65, 1.0)==1);
-  printf("\nCheck 7\n");
-  assert(batteryIsOk(-20, 90, 0.6)==1);
-  printf("\nCheck 8\n");
-  assert(batteryIsOk(-40, 24, 0.5)==1);
-  printf("\nCheck 9\n");
-  assert(batteryIsOk(43, 77, 0.78)==0);
-  printf("\nCheck 10\n");
-  assert(batteryIsOk(2, 24, 0.03)==0);
+  assert();
 }
 
  
