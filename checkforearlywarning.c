5c97ebe21006997367aba5dc920444f2bb5f997b
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 16:17:10 +0530

    Update checkforearlywarning.c

--- checkforearlywarning.c
+++ checkforearlywarning.c
@@ -4,7 +4,7 @@ void earlywarning(int invalid,float temperature, float soc, float chargeRate)
   if(invalid!=1)
   {
   printf("Though values are within range, check for early warning if any\n");
-  printwarning(temperature,soc,chargeRate);
+  printbatterystatus(temperature,soc,chargeRate);
   }
   else
   printf("Battery status not okay\n");
