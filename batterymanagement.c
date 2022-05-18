1ca63a603113b89df325228ae25457dd0a8d64f0
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 17:44:09 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -5,28 +5,29 @@ enum languages{English=1,German=2};
 enum languages l;
 
 int CheckBatteryStatus(float temperature, float soc, float chargeRate) {
+  int isTemperatureInvalid=0;
+  int isSocInvalid=0;
+  int isChargerateInvalid=0;
+  int invalid=0;
   if(l==1){
   printf("Temperature ");
-  int isTemperatureInvalid = checkvaluebreach(temperature,0,45);
+  isTemperatureInvalid = checkvaluebreach(temperature,0,45);
   printf("State of Charge ");
-  int isSocInvalid = checkvaluebreach(soc,20,80);
+  isSocInvalid = checkvaluebreach(soc,20,80);
   printf("Charge Rate ");
-  int isChargerateInvalid= checkvaluebreach(chargeRate,0,0.8);
-  int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
-  earlywarning(invalid,temperature,soc,chargeRate);
-  return invalid;
+  isChargerateInvalid= checkvaluebreach(chargeRate,0,0.8);
   }
   if(l==2){
   printf("Temperatur ");
-  int isTemperatureInvalid = checkvaluebreach(temperature,0,45);
+  isTemperatureInvalid = checkvaluebreach(temperature,0,45);
   printf("Ladezustand ");
-  int isSocInvalid = checkvaluebreach(soc,20,80);
+  isSocInvalid = checkvaluebreach(soc,20,80);
   printf("Ladestrom ");
-  int isChargerateInvalid= checkvaluebreach(chargeRate,0,0.8);
-  int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
+  isChargerateInvalid= checkvaluebreach(chargeRate,0,0.8);
+  } 
+  invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
   earlywarning(invalid,temperature,soc,chargeRate);
   return invalid;
-  } 
 }
 
 int main() {
