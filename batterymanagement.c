3b1a7621318518002e0261bb0194b3582cb3daad
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 15:28:14 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -4,16 +4,10 @@
 int CheckBatteryStatus(float temperature, float soc, float chargeRate) {
   printf("Temperature ");
   int isTemperatureInvalid = checklimit(temperature,0,45);
-  int isTempApproachinglimit = warning(temperature,0,45);
-  (isTempApproachinglimit==1) ? printf("Warning: Battery Temperature approaching lower limit\n") : ((isTempApproachinglimit==2)? printf("Warning: Battery Temperature approaching higher limit\n") : printf(""));
   printf("State of Charge ");
   int isSocInvalid = checklimit(soc,20,80);
-  int isSocApproachinglimit = warning(soc,20,80);
-  (isSocApproachinglimit==1) ? printf("Warning: Battery Approaching discharge\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Approaching charge-peak\n") : printf(""));
   printf("Charge Rate ");
   int isChargerateInvalid= checklimit(chargeRate,0,0.8);
-  int isChargerateApproachinglimit = warning(chargeRate,0,0.8);
-  (isChargerateApproachinglimit==1) ? printf("Warning: Battery Charge rate approaching lower limit\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Charge rate approaching higher limit\n") : printf(""));
   int invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
   return invalid;
 }
