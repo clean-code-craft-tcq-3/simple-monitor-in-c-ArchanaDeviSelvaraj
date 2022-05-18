01b0e435a6a150adf58d0ac7b81e0bbaf8bd5200
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 18:08:56 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -9,25 +9,47 @@ int CheckBatteryStatus(float temperature, float soc, float chargeRate) {
   int isSocInvalid=0;
   int isChargerateInvalid=0;
   int invalid=0;
-  if(l==1){
-  printf("Temperature ");
-  isTemperatureInvalid = checkvaluebreach(temperature,0,45);
-  printf("State of Charge ");
-  isSocInvalid = checkvaluebreach(soc,20,80);
-  printf("Charge Rate ");
-  }
-  if(l==2){
-  printf("Temperatur ");
-  isTemperatureInvalid = checkvaluebreach(temperature,0,45);
-  printf("Ladezustand ");
-  isSocInvalid = checkvaluebreach(soc,20,80);
-  printf("Ladestrom ");
-  } 
-  isChargerateInvalid= checkvaluebreach(chargeRate,0,0.8);
+  isTemperatureInvalid = TempCheck(temperature);
+  isSocInvalid = SocCheck(soc);
+  isChargerateInvalid = ChargerateCheck(chargeRate);
   invalid = isTemperatureInvalid||isSocInvalid||isChargerateInvalid;
   earlywarning(invalid,temperature,soc,chargeRate);
   return invalid;
+ }
+
+int Tempcheck(float temperature)
+{
+  if(l==1)
+  printf("Temperature ");
+  if(l==2){
+  printf("Temperatur ");
+  isTemperatureInvalid = checkvaluebreach(temperature,0,45);
+  return isTemperatureInvalid;
 }
+  
+int SocCheck(float soc)
+{
+  if(l==1)
+  printf("State of Charge ");
+  if(l==2){
+  printf("Ladezustand ");
+  isSocInvalid = checkvaluebreach(soc,20,80);
+  return isSocInvalid;
+}
+  
+int ChargerateCheck(float soc)
+{
+  if(l==1)
+  printf("Charge Rate ");
+  if(l==2){
+  printf("Ladestrom ");
+  isChargerateInvalid = checkvaluebreach(soc,20,80);
+  return isChargerateInvalid;
+}
+
+    
+    
+
 
 int main() {
   l=1;
