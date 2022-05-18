07ebe89f004d20037eea32378c6c22b09018a51e
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 15:39:39 +0530

    Update printwarning.c

--- printwarning.c
+++ printwarning.c
@@ -4,7 +4,7 @@ void printwarning(float temperature, float soc, float chargeRate)
 int isTempApproachinglimit = warning(temperature,0,45);
 (isTempApproachinglimit==1) ? printf("Warning: Battery Temperature approaching lower limit\n") : ((isTempApproachinglimit==2)? printf("Warning: Battery Temperature approaching higher limit\n") : printf("No early warning"));
 int isSocApproachinglimit = warning(soc,20,80);
-(isSocApproachinglimit==1) ? printf("Warning: Battery Approaching discharge\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Approaching charge-peak\n") : printf(""));
+(isSocApproachinglimit==1) ? printf("Warning: Battery Approaching discharge\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Approaching charge-peak\n") : printf("No early warning"));
 int isChargerateApproachinglimit = warning(chargeRate,0,0.8);
 (isChargerateApproachinglimit==1) ? printf("Warning: Battery Charge rate approaching lower limit\n") : ((isSocApproachinglimit==2)? printf("Warning: Battery Charge rate approaching higher limit\n") : printf("No early warning"));
 }     
