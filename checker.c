31c8bac9a0ba2f4da871b6be2c0b02de5d31d67c
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-18 10:47:01 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-void warning(float *value,float minlimit,float maxlimit)
+void warning(float value,float minlimit,float maxlimit)
 {
   float tolerance=(5.000000/100.000000)*maxlimit;
   if (value == minlimit||value == minlimit+tolerance) {
@@ -12,7 +12,7 @@ void warning(float *value,float minlimit,float maxlimit)
   }
 }
 
-int checklimit(float *value,float minlimit,float maxlimit)
+int checklimit(float value,float minlimit,float maxlimit)
 {
   int alertcount=0;
   if (value < minlimit) {
@@ -31,7 +31,7 @@ int checklimit(float *value,float minlimit,float maxlimit)
  return alertcount;
 }
 
-int batteryIsOk(float *temperature, float *soc, float *chargeRate) {
+int batteryIsOk(float temperature, float soc, float chargeRate) {
   printf("Temperature ");
   int isTemperatureInvalid = checklimit(temperature,0,45);
   printf("State of Charge ");
