c178fbeb174f4915c57731e3f69e9b7aaee25bb8
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 11:30:19 +0530

    Update batterymanagement.h

--- batterymanagement.h
+++ batterymanagement.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+enum languages{English=1,German=2};
+enum languages l;
 int CheckBatteryStatus(float temperature, float soc, float chargeRate);
 int warning(float value,float minlimit,float maxlimit);
 int isValuebreached(float value,float minlimit,float maxlimit);
