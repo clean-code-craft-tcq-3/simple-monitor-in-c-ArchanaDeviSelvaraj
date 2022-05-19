3785ee388568ccd2e8fd85686cedf964343d2e37
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 12:55:11 +0530

    Update checkforearlywarning.c

--- checkforearlywarning.c
+++ checkforearlywarning.c
@@ -1,5 +1,21 @@
 #include "batterymanagement.h"
 
+int warning(float value,float minlimit,float maxlimit)
+{
+  float tolerance=(5.000000/100.000000)*maxlimit;
+  if(minlimit<=value<=minlimit+tolerance)
+  {
+    return 1;
+    //1-Approaching lower limit
+    }
+  else if(maxlimit-tolerance<=value<=maxlimit){
+    return 2;
+    //2-Approaching upper limit
+    }
+  else
+    return 0;
+}
+
 void printmessage()
 {
     if(l==1)
