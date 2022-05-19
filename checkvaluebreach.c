2f12ddb5958d5226c2d60da4f880a643dad23470
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 10:44:12 +0530

    Update checkvaluebreach.c

--- checkvaluebreach.c
+++ checkvaluebreach.c
@@ -5,14 +5,17 @@ int isValuebreached(float value,float minlimit,float maxlimit)
   if (value < minlimit) {
     printf("value is below the limit\n");
     alertcount=1;
+    // alertcount=1 means value breached
   }
   else if (value > maxlimit){
     printf("value is above the limit\n");
     alertcount=1;
+    // alertcount=1 means value breached
   }
   else
   {
     printf("value is within range\n");
+    // alertcount=0 means value within range
   }
  return alertcount;
 }
