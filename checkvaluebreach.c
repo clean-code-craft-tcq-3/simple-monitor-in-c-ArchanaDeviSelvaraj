74c037d4d4ff958c550f33fc037e655c1398c8f1
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 11:38:59 +0530

    Update checkvaluebreach.c

--- checkvaluebreach.c
+++ checkvaluebreach.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 #include "batterymanagement.h"
 
+void printlowlimitbreach()
+{
+    if(l==1)
+    printf("value is below the limit\n");
+    if(l==2)
+    printf("Der Wert liegt unter dem Grenzwert\n");
+}
+void printhighlimitbreach()
+{
+    if(l==1)
+    printf("value is above the limit\n");
+    if(l==2) 
+    printf("Der Wert liegt über dem Limit\n");
+}
+void printnobreach()
+{
+      if(l==1)
+      printf("value is within range\n");
+      if(l==2) 
+      printf("Wert liegt im Bereich\n");
+}
 int isValuebreached(float value,float minlimit,float maxlimit)
 {
   int alertcount=0;
   if (value < minlimit) 
   {
-    if(l==1)
-    printf("value is below the limit\n");
-    if(l==2)
-    printf("Der Wert liegt unter dem Grenzwert\n");
+    printlowlimitbreach();
     alertcount=1;
     // alertcount=1 means value breached
   }
   else if (value > maxlimit)
   {
-    if(l==1)
-    printf("value is above the limit\n");
-    if(l==2) 
-    printf("Der Wert liegt über dem Limit\n");
+    printhighlimitbreach();
     alertcount=1;
     // alertcount=1 means value breached
   }
     else
     {
-      if(l==1)
-      printf("value is within range\n");
-      if(l==2) 
-      printf("Wert liegt im Bereich\n");
+      printnobreach();
       // alertcount=0 means value within range
     }
  return alertcount;
