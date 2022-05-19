38530faf9882d5577be2682b1ab3cad040fdca50
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 10:42:06 +0530

    Update batterymanagement.h

--- batterymanagement.h
+++ batterymanagement.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int CheckBatteryStatus(float temperature, float soc, float chargeRate);
 int warning(float value,float minlimit,float maxlimit);
-int checkvaluebreach(float value,float minlimit,float maxlimit);
+int isValuebreached(float value,float minlimit,float maxlimit);
 void printbatterystatus(float temperature, float soc, float chargeRate);
 void earlywarning(int invalid,float temperature, float soc, float chargeRate);
