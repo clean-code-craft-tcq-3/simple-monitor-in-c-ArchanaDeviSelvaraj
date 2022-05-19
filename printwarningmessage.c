91ad0b06e61415363ec5686e2980b77fdc8d2c3c
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 12:19:08 +0530

    Update printwarningmessage.c

--- printwarningmessage.c
+++ printwarningmessage.c
@@ -1,1 +1,10 @@
+void printtempwarningL1(int isTempApproachingLimit)
+{
+(isTempApproachingLimit==1) ? printf("Warning: Battery Temperature approaching lower limit\n") : ((isTempApproachingLimit==2)? printf("Warning: Battery Temperature approaching higher limit\n") : printf("No early warning for temperature\n"));
+}
+
+void printtempwarningL2(int isTempApproachingLimit)
+{
+(isTempApproachingLimit==1) ? printf("Warnung: Batterietemperatur nähert sich dem unteren Grenzwert\n") : ((isTempApproachingLimit==2)? printf("Warnung: Batterietemperatur nähert sich dem oberen Grenzwert\n") : printf("Keine Frühwarnung für Temperature\n"));
+}
 
