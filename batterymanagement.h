ad3cc63f9983d5910deb074467e3a732789aaa25
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-19 15:12:40 +0530

    Update batterymanagement.h

--- batterymanagement.h
+++ batterymanagement.h
@@ -23,5 +23,6 @@ void printchargeratewarning(int isChargerateApproachingLimit);
 void isTempApproachinglimit(float temperature);
 void isSocApproachinglimit(float soc);
 void isChargerateApproachinglimit(float chargeRate);
+int checklimitforwarning(float value,float minlimit,float maxlimit)
 void test();
 
